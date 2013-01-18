a5a725fd249a779b06f6d7158297310d92267127
Maxim Grach <mvgrach@gmail.com>
2013-01-19 02:46:32 +0600

    Repi init

---
+++ assets/www/js/zepto.gallery.locale.js
@@ -0,0 +1,58 @@
+;(function($){
+	$.extend($.fn.galleryLocale, {
+		zh: {
+			about: '关于应用程序',
+			favorite: '我的最爱',
+			by_date: '最新',
+			by_rank: '最好',
+			by_interest: '辩论',
+			noise: '随机',
+			btn_menu: '菜单'
+		},
+		de: {
+			about: 'Über App',
+			favorite: 'Favoriten',
+			by_date: 'Neueste',
+			by_rank: 'Die besten',
+			by_interest: 'Debating',
+			noise: 'Zufällig',
+			btn_menu: 'Menü'
+		},
+		es: {
+			about: 'Acerca app',
+			favorite: 'Favoritos',
+			by_date: 'Más reciente',
+			by_rank: 'Lo mejor',
+			by_interest: 'Debate',
+			noise: 'Azar',
+			btn_menu: 'Menú'
+		},
+		ja: {
+			about: 'ほど',
+			favorite: 'お気に入り',
+			by_date: '最新',
+			by_rank: 'トップ',
+			by_interest: 'ディベート',
+			noise: 'ランダム',
+			btn_menu: 'メニュー'
+		},
+		fr: {
+			about: 'À propos',
+			favorite: 'Favoris',
+			by_date: 'Dernière',
+			by_rank: 'Le mieux',
+			by_interest: 'Débattre',
+			noise: 'Aléatoire',
+			btn_menu: 'Menu'
+		},
+		ru: {
+			about: 'О приложении',
+			favorite: 'Избранное',
+			by_date: 'Последние',
+			by_rank: 'Лучшие',
+			by_interest: 'Спорные',
+			noise: 'Случайные',
+			btn_menu: 'Меню'
+		}
+	});
+})(Zepto)
